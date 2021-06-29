4a22b79fe39d734522926b2338e7183a331a88d7
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-06-29 17:15:10 +0300

    header

--- components/Buttons/RainbowButton/RainbowButton.jsx
+++ components/Buttons/RainbowButton/RainbowButton.jsx
@@ -1,5 +1,17 @@
-const RainbowButton = () => {
-    
+import PropTypes from "prop-types";
+import Link from "next/link";
+
+const RainbowButton = ({ path, text }) => {
+  return (
+    <Link href={path}>
+      <a>{text}</a>
+    </Link>
+  );
+};
+
+RainbowButton.propTypes = {
+  path: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default RainbowButton;
--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import navPaths from "@/utils/paths";
 import styles from "./Header.module.scss";
+import RainbowButton from "../Buttons/RainbowButton/RainbowButton";
 
 const Header = ({ header }) => {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
+  const handleClickBurger = () => {
+    setBurgerStatus(!burgerStatus);
+  };
+
   const { about, contact, courses, home } = navPaths;
+
   return (
     <header className={styles.header}>
-      <div className={styles.header__logo}>
+      <div className={styles.header__logo}></div>
+      <div
+        onClick={() => handleClickBurger()}
+        className={`${styles.burgerContainer} `}
+      >
+        <div
+          className={`${styles.hamburgerToClose} ${
+            burgerStatus ? styles.close : ""
+          }`}
+        >
+          <div className={styles.hamburgerMiddle} />
+        </div>
       </div>
-      <nav className={styles.header__nav}>
+      <nav className={`${styles.header__nav} ${burgerStatus ? styles.header__nav_open : ''}`}>
         <ul className={styles.list}>
+          {/* <li className={styles.list__item}>
+            <RainbowButton path={home} text={'Главная'}/>
+          </li>
+          <li className={styles.list__item}>
+            <RainbowButton path={courses} text={'Курсы'}/>
+          </li>
+          <li className={styles.list__item}>
+            <RainbowButton path={about} text={'О школе'}/>
+          </li>
+          <li className={styles.list__item}>
+            <RainbowButton path={contact} text={'Контакты'}/>
+          </li> */}
           <li className={styles.list__item}>
             <Link href={home}>
               <a className={styles.list__item__text}>Главная</a>
