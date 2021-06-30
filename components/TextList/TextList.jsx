2d99f44abc5eed3620ae798908810e4ee34add24
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-06-30 16:06:21 +0300

    list

--- components/TextList/TextList.jsx
+++ components/TextList/TextList.jsx
@@ -5,6 +5,7 @@ import styles from "./TextList.module.scss";
 const TextList = ({title, list}) => {
   return (
     <div className={styles.list}>
+      <div className={styles.list__robots}/>
       <h2 className={styles.list__header}>{title}</h2>
       <ul className={styles.list__items}>
         {list.map((item) => <Item text={item}/>)}
