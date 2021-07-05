38c1bf55efa511d67ebe59a3504a8e1e307bbafd
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-07-05 13:06:22 +0300

    img fix

--- pages/courses.js
+++ pages/courses.js
@@ -62,7 +62,7 @@ export async function getStaticProps() {
           id: 2,
           title: 'Lego WeDo',
           asPath: 'LegoWeDo',
-          logo: ['/static/new_img/courses/lego.png'],
+          logo: ['/static/new_img/courses/lego_white.png'],
           description:
               'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate',
         },
