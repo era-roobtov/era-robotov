0c72ddd2d140755e50c06535f998e81b8042d3ce
dima <psiniy-programmer@yandex.ru>
2021-08-25 01:35:14 +0300

    Квадраты первой секции главной

--- pages/index.js
+++ pages/index.js
@@ -15,122 +15,52 @@ import {
 import mergeStyles from '@/utils/mergeStyles';
 import {useEffect, useState} from "react";
 
-const date = new Date();
-const openDate = new Date(2021, 8, 1, 0, 0, 0);
-const openObj = {
-    h: openDate.getHours(),
-
-}
-console.log(openDate)
-
-let endT = {
-    day: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0
-}
-
-function timer() {
-    var nowDate = new Date();
-    var achiveDate = new Date(2021, 8, 1, 0, 0, 0); //Задаем дату, к которой будет осуществляться обратный отсчет
-    var result = (achiveDate - nowDate) + 1000;
-    if (result < 0) {
-        var elmnt = document.getElementById('timer');
-        elmnt.innerHTML = ' - : - - : - - : - - ';
-        return undefined;
-    }
-    endT = {
-        day: Math.floor(result / 1000 / 60 / 60 / 24),
-        hours: Math.floor((result / 1000 / 60 / 60) % 24),
-        minutes: Math.floor((result / 1000 / 60) % 60),
-        seconds: Math.floor((result / 1000) % 60)
-    }
-    // var seconds = Math.floor((result/1000)%60);
-    // var minutes = Math.floor((result/1000/60)%60);
-    // var hours = Math.floor((result/1000/60/60)%24);
-    // var days = Math.floor(result/1000/60/60/24);
-    if (endT.seconds < 10) endT.seconds = '0' + endT.seconds;
-    if (endT.minutes < 10) endT.minutes = '0' + endT.minutes;
-    if (endT.hours < 10) endT.hours = '0' + endT.hours;
-    console.log('timer >>> ', endT)
-    // elmnt.innerHTML = days + ':' + hours + ':' + minutes + ':' + seconds;
-    setTimeout(timer, 1000);
-}
-
-timer();
-
-
-const Index = () => {
-    const [timer, setTimer] = useState({
-        h: date.getHours(),
-        min: date.getMinutes(),
-        sec: date.getSeconds()
-    });
-
-    useEffect(() => {
-        setInterval(() => {
-            // console.log('k')
-            let nD = new Date();
-            setTimer({
-                h: nD.getHours(),
-                min: nD.getMinutes(),
-                sec: nD.getSeconds()
-            })
-        }, 1000)
-    }, [])
-
-    useEffect(() => {
-        // console.log(timer)
-    }, [timer])
+const Index = ({textList, advantagesList, aboutUsList}) => {
+    const str = [
+        'Место погружения в мир инновационных технологий и развития потенциала вашего ребенка',
+    ];
 
     return (
-        <section className={styles.temp}>
-            <img src={bigLogo} alt="Эра роботов"/>
-            <div>
-                <p>Сайт откроется через: </p>
-                <p>{endT.day} Дней {endT.hours} часов {endT.minutes} минут {endT.seconds} секунд</p>
-            </div>
-        </section>
-    )
-}
+        <div className="app">
+            <Head>
+                <title>Эра роботов</title>
+                <meta name="description" content="Школа робототехники Эра роботов"/>
+            </Head>
+            <Layout>
+                <Greeting
+                    header="Школа программирования и робототехники Эра Роботов"
+                    btn={{show: true, btnText: 'Выбрать курс'}}
+                    subText={str}
+                    imgPath="/static/new_img/greeting/robots.png"
+                    mobileImgTop={false}
+                />
+                <div className='orange__boxes_top1'>
+                    <div className='orange__box_top'>
+                        <section className={`${styles.aboutUs} ${CLIP_BLUE}`}>
+                            <h3>О нас</h3>
+                            <p>
+                                Наша Школа "ЭраРоботов" собрала единомышленников, замечательных преподавателей в области
+                                робототехники и программирования. Наши сотрудники имеют высшее техническое образование и уже
+                                немалый опыт работы со слушателями разных возрастов, умеют доступно и интересно преподнести
+                                информацию. В процессе обучения мы используем новейшее оборудование, качественные наборы для
+                                сборки роботов и современное программное обеспечение.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+                <div className={styles.robots}/>
+                <TextList shiftClass={SHIFT_SINGLE} {...textList} clip={CLIP_ORANGE}/>
+                <Advantages
+                    columnCount={1}
+                    shiftClass={SHIFT_DOUBLE} {...advantagesList}
+                    showBtn={false} clip={CLIP_BLUE}
+                />
+                <Footer shiftCount='mobileSingle-pcDouble'/>
+            </Layout>
+        </div>
+    );
+};
 
-// const Index = ({textList, advantagesList, aboutUsList}) => {
-//     const str = [
-//         'Место погружения в мир инновационных технологий и развития потенциала вашего ребенка',
-//     ];
-//
-//     return (
-//         <div className="app">
-//             <Head>
-//                 <title>Эра роботов</title>
-//                 <meta name="description" content="Школа робототехники Эра роботов"/>
-//             </Head>
-//             <Layout>
-//                 <Greeting
-//                     header="Школа программирования и робототехники Эра Роботов"
-//                     btn={{show: true, btnText: 'Выбрать курс'}}
-//                     subText={str}
-//                     imgPath="/static/new_img/greeting/robots.png"
-//                     mobileImgTop={false}
-//                 />
-//                 <section className={`${styles.aboutUs} ${CLIP_BLUE}`}>
-//                     <h3>О нас</h3>
-//                     <p>
-//                         Наша Школа "ЭраРоботов" собрала единомышленников, замечательных преподавателей в области робототехники и программирования. Наши сотрудники имеют высшее техническое образование и уже немалый опыт работы со слушателями разных возрастов, умеют доступно и интересно преподнести информацию. В процессе обучения мы используем новейшее оборудование, качественные наборы для сборки роботов и современное программное обеспечение.
-//                     </p>
-//                 </section>
-//                 <div className={styles.robots}/>
-//                 <TextList shiftClass={SHIFT_SINGLE} {...textList} clip={CLIP_ORANGE}/>
-//                 <Advantages
-//                     columnCount={1}
-//                     shiftClass={SHIFT_DOUBLE} {...advantagesList}
-//                     showBtn={false} clip={CLIP_BLUE}
-//                 />
-//                 <Footer shiftCount='mobileSingle-pcDouble'/>
-//             </Layout>
-//         </div>
-//     );
-// };
 
 export async function getStaticProps() {
     return {
@@ -205,3 +135,58 @@ export async function getStaticProps() {
 }
 
 export default Index;
+
+// timer page
+// const date = new Date();
+// const openDate = new Date(2021, 8, 1, 0, 0, 0);
+// const openObj = {
+//     h: openDate.getHours(),
+//
+// }
+// console.log(openDate)
+//
+// let endT = {
+//     day: 0,
+//     hours: 0,
+//     minutes: 0,
+//     seconds: 0
+// }
+//
+// function timer() {
+//     var nowDate = new Date();
+//     var achiveDate = new Date(2021, 8, 1, 0, 0, 0); //Задаем дату, к которой будет осуществляться обратный отсчет
+//     var result = (achiveDate - nowDate) + 1000;
+//     if (result < 0) {
+//         var elmnt = document.getElementById('timer');
+//         elmnt.innerHTML = ' - : - - : - - : - - ';
+//         return undefined;
+//     }
+//     endT = {
+//         day: Math.floor(result / 1000 / 60 / 60 / 24),
+//         hours: Math.floor((result / 1000 / 60 / 60) % 24),
+//         minutes: Math.floor((result / 1000 / 60) % 60),
+//         seconds: Math.floor((result / 1000) % 60)
+//     }
+//     // var seconds = Math.floor((result/1000)%60);
+//     // var minutes = Math.floor((result/1000/60)%60);
+//     // var hours = Math.floor((result/1000/60/60)%24);
+//     // var days = Math.floor(result/1000/60/60/24);
+//     if (endT.seconds < 10) endT.seconds = '0' + endT.seconds;
+//     if (endT.minutes < 10) endT.minutes = '0' + endT.minutes;
+//     if (endT.hours < 10) endT.hours = '0' + endT.hours;
+//     console.log('timer >>> ', endT)
+//     // elmnt.innerHTML = days + ':' + hours + ':' + minutes + ':' + seconds;
+//     setTimeout(timer, 1000);
+// }
+//
+// timer();
+
+// return (
+//     <section className={styles.temp}>
+//         <img src={bigLogo} alt="Эра роботов"/>
+//         <div>
+//             <p>Сайт откроется через: </p>
+//             <p>{endT.day} Дней {endT.hours} часов {endT.minutes} минут {endT.seconds} секунд</p>
+//         </div>
+//     </section>
+// )
